b49b43e56263025b61f1fb566c7eaf2eecc0bd7d
tetsuyaokuyama0312 <y227827@gmail.com>
2020-01-12 17:50:07 +0900

    Kotlin Android Extensions を導入

--- QiitaClient/app/src/main/kotlin/com/example/qiitaclient/view/ArticleView.kt
+++ QiitaClient/app/src/main/kotlin/com/example/qiitaclient/view/ArticleView.kt
@@ -1,17 +1,13 @@
 package com.example.qiitaclient.view
 
 import android.content.Context
-import android.graphics.Color
-import android.media.Image
 import android.util.AttributeSet
 import android.view.LayoutInflater
 import android.widget.FrameLayout
-import android.widget.ImageView
-import android.widget.TextView
 import com.bumptech.glide.Glide
 import com.example.qiitaclient.R
-import com.example.qiitaclient.bindView
 import com.example.qiitaclient.model.Article
+import kotlinx.android.synthetic.main.view_article.view.*
 
 class ArticleView : FrameLayout {
 
@@ -25,19 +21,13 @@ class ArticleView : FrameLayout {
     constructor(context: Context, attrs: AttributeSet, defStyleAttr: Int, defStyleRes: Int) :
             super(context, attrs, defStyleAttr, defStyleRes)
 
-    private val profileImageView: ImageView by bindView(R.id.profile_image_view)
-
-    private val titleTextView: TextView by bindView(R.id.title_text_view)
-
-    private val userNameTextView: TextView by bindView(R.id.user_name_text_view)
-
     init {
         LayoutInflater.from(context).inflate(R.layout.view_article, this)
     }
 
     fun setArticle(article: Article) {
-        titleTextView.text = article.title
-        userNameTextView.text = article.user.name
-        Glide.with(context).load(article.user.profileImageUrl).into(profileImageView)
+        title_text_view.text = article.title
+        user_name_text_view.text = article.user.name
+        Glide.with(context).load(article.user.profileImageUrl).into(profile_image_view)
     }
 }
